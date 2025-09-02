c0dbc328a7580fc849270fac88f09daea91cbc08
Marlon Lede <marlonlede@gmail.com>
2025-09-02 15:40:25 +0200

    setup spring boot be project

---
+++ backend/src/main/java/nl/justid/todobackend/TodoBackend.java
@@ -0,0 +1,22 @@
+package nl.justid.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TodoBackend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackend.class, args);
+	}
+
+    @RequestMapping("/")
+    public ResponseEntity<String> mainRoot() {
+        return ResponseEntity.ok("Hello TODO applicatie");
+    }
+
+}
