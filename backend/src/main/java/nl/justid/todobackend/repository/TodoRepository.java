78ba763b28cb3601e521121628b141460fd1ad70
Marlon Lede <marlonlede@gmail.com>
2025-09-02 20:09:41 +0200

    feat(todo): remove redundant method findAllBy

--- backend/src/main/java/nl/justid/todobackend/repository/TodoRepository.java
+++ backend/src/main/java/nl/justid/todobackend/repository/TodoRepository.java
@@ -3,9 +3,6 @@ package nl.justid.todobackend.repository;
 import nl.justid.todobackend.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
-public interface TodoRepository extends JpaRepository<Todo, UUID> {
-    Optional<Todo> findAllBy(UUID id);
-}
+public interface TodoRepository extends JpaRepository<Todo, UUID> {}
