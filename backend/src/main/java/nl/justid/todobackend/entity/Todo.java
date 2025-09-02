15355a6698739517c7422f5d701f183600d972d0
Marlon Lede <marlonlede@gmail.com>
2025-09-02 18:12:20 +0200

    create entity todo

---
+++ backend/src/main/java/nl/justid/todobackend/entity/Todo.java
@@ -0,0 +1,36 @@
+package nl.justid.todobackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.*;
+import java.util.*;
+
+//@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private Set<String> tags = new HashSet<>();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private OffsetDateTime createdAt;
+
+    private LocalDate deadline;
+
+    private boolean completed;
+}
