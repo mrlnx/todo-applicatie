4f3a21efc5c0c8acbd4cb1bd6b8bfab2c68fefd6
Marlon Lede <marlonlede@gmail.com>
2025-09-03 13:20:40 +0200

    feat(ui): style ui create todo

--- frontend/src/app/pages/create-todo/create-todo.ts
+++ frontend/src/app/pages/create-todo/create-todo.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-create-todo',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatChipsModule,
+    MatIconModule,
+  ],
   templateUrl: './create-todo.html',
-  styleUrl: './create-todo.scss'
+  styleUrl: './create-todo.scss',
+  providers: [provideNativeDateAdapter()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateTodo {
+  readonly templateKeywords = signal(['angular', 'how-to', 'tutorial', 'accessibility']);
 
+  announcer = inject(LiveAnnouncer);
+
+  removeTemplateKeyword(keyword: string) {
+    this.templateKeywords.update((keywords) => {
+      const index = keywords.indexOf(keyword);
+      if (index < 0) {
+        return keywords;
+      }
+
+      keywords.splice(index, 1);
+      this.announcer.announce(`removed ${keyword} from template form`);
+      return [...keywords];
+    });
+  }
+
+  addTemplateKeyword(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.templateKeywords.update((keywords) => [...keywords, value]);
+      this.announcer.announce(`added ${value} to template form`);
+    }
+
+    event.chipInput!.clear();
+  }
 }
