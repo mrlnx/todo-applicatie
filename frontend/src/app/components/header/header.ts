3e226686c1d1fcf2083a3525a738407878454d51
Marlon Lede <marlonlede@gmail.com>
2025-09-03 20:10:41 +0200

    feat(fe logic): add todo count to header

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { TodoStore } from '../../service/todo.store';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, AsyncPipe, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header implements OnInit {
+  constructor(public store: TodoStore) {}
+
+  ngOnInit(): void {
+    this.store.load();
+  }
+}
