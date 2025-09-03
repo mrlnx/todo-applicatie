2bb0118da121241853c0eb1029c79d179689ce38
Marlon Lede <marlonlede@gmail.com>
2025-09-03 15:35:13 +0200

    feat(ui): style ui manage todo

--- frontend/src/app/pages/manage-todo/manage-todo.ts
+++ frontend/src/app/pages/manage-todo/manage-todo.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-manage-todo',
-  imports: [],
+  imports: [MatChipsModule, MatSlideToggleModule, MatButtonModule],
   templateUrl: './manage-todo.html',
-  styleUrl: './manage-todo.scss'
+  styleUrl: './manage-todo.scss',
 })
-export class ManageTodo {
-
-}
+export class ManageTodo {}
